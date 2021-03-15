a661f7750c8c6c2a2e332deb37eb07e487e5156c
Vlad <odincov.vlad76@gmail.com>
2021-03-15 16:53:46 +0300

    1.0.1

--- main.py
+++ main.py
@@ -7,14 +7,6 @@ from config import TOKEN
 from handlers.handler_registrator import register_handlers
 
 # подключаем логирование
-# рекомендуется уровень логиование: INFO
-# f_handler = logging.FileHandler(filename='log.txt', mode="w", encoding='utf-8')
-#
-# logging.Handler = f_handler
-# f_format = logging.Formatter('%(asctime)s - %(message)s')
-# f_handler.setFormatter(f_format)
-# logging.getLogger().addHandler(f_handler)
-
 logging.basicConfig(level=logging.DEBUG)
 
 # создаем бота и диспатчер
